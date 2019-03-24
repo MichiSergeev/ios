17b058fdb70a463c8bc8fe22242fe11bcebb37b3
Michael <michael.sergeew@gmail.com>
2019-03-24 19:50:39 +0400

    Solution Pangrams

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -4,7 +4,23 @@
 
 // Complete the pangrams function below.
 - (BOOL)pangrams:(NSString *)string {
-    return NO;
+    
+    for (int index=0; index<26; index+=1) {
+        
+        NSString *abc=@"abcdefghijklmnopqrstuvwxyz";
+        char temp;
+        temp=[abc characterAtIndex:index];
+        NSString *tempString=[NSString stringWithFormat:@"%c", temp];
+        NSString *stringInputLowerCase=[string lowercaseString];
+        
+        if ([stringInputLowerCase containsString:tempString]==0) {
+            return 0;
+        }
+        
+    }
+    
+    
+    return 1;
 }
 
 @end
