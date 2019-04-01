f87dab7a363c631224ffcc0a33d1684abae6f7af
Michael <michael.sergeew@gmail.com>
2019-04-01 19:10:20 +0400

    Solution KidnapperNote

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -5,11 +5,23 @@
 
 -(BOOL) checkMagazine:(NSString *)magaine note:(NSString *)note {
     
+    int hit=0;
+    NSMutableArray *noteArray=[NSMutableArray new];
+    NSMutableArray *magaineArray=[NSMutableArray new];
+    noteArray=[[note lowercaseString] componentsSeparatedByString:@" "];
+    magaineArray=[[magaine lowercaseString] componentsSeparatedByString:@" "];
+    
+    for (int i=0; i<noteArray.count; i+=1) {
+        for (int j=0; j<magaineArray.count; j+=1) {
+            if (noteArray[i]==magaineArray[j]) {
+                hit+=1;
+                [magaineArray removeObjectAtIndex:j];
+                break;
+            }
+        }
+    }
+    
+    return hit==noteArray.count ? YES:NO;
 }
-
-
-
-
-
 
 @end
