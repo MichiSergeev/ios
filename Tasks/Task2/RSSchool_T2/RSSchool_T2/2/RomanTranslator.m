44769dfa1355287d5a9d586b79c9457db2a83210
Michael <michael.sergeew@gmail.com>
2019-04-06 21:28:01 +0400

    Solution RomanTranslator

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/2/RomanTranslator.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/2/RomanTranslator.m
@@ -51,9 +51,77 @@
     
 }
 
-/*
+
 -(NSString*) arabicFromRoman:(NSString *)romanString {
     
+    NSDictionary *romanArabicDictionary=@{ @"M":@(1000),
+                                           @"CM":@(900),
+                                           @"DCCC":@(800),
+                                           @"DCC":@(700),
+                                           @"DC":@(600),
+                                           @"D":@(500),
+                                           @"CD":@(400),
+                                           @"CCC":@(300),
+                                           @"CC":@(200),
+                                           @"C":@(100),
+                                           @"XC":@(90),
+                                           @"LXXX":@(80),
+                                           @"LXX":@(70),
+                                           @"LX":@(60),
+                                           @"L":@(50),
+                                           @"XL":@(40),
+                                           @"XXX":@(30),
+                                           @"XX":@(20),
+                                           @"X":@(10),
+                                           @"IX":@(9),
+                                           @"VIII":@(8),
+                                           @"VII":@(7),
+                                           @"VI":@(6),
+                                           @"V":@(5),
+                                           @"IV":@(4),
+                                           @"III":@(3),
+                                           @"II":@(2),
+                                           @"I":@(1)};
+    
+    int lenght=(int)[romanString length];
+    int resultSum=0;
+    
+    for (int i=0; i<=lenght-1; i+=1) {
+        
+        NSMutableString *tempString=[[NSMutableString alloc]init];
+        
+        for (int j=i; j<=lenght-1; j+=1) {
+            
+            int tempSum;
+            [tempString appendFormat:@"%c",[romanString characterAtIndex:j]];
+            NSMutableString *key=[[NSMutableString alloc]initWithString:tempString];
+            
+            
+            if ([romanArabicDictionary objectForKey:key]) {
+                
+                int arabicValueInDictionary=0;
+                arabicValueInDictionary=[[romanArabicDictionary objectForKey:key]intValue];
+                tempSum=arabicValueInDictionary;
+                
+                if (j==lenght-1) {
+                    resultSum+=tempSum;
+                    i=j;
+                }
+                
+            } else {
+                i=j-1;
+                resultSum+=tempSum;
+                break;
+            }
+            
+        }
+        
+        [tempString release];
+    }
+    
+    return [NSString stringWithFormat:@"%i",resultSum];
 }
-*/
+
+
+
 @end
